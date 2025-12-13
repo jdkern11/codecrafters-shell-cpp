9b0cab89a1fe5ccbc6ce0c40a9ba29c50c2b70e6
Joe Kern <josephdanielkern@gmail.com>
2025-12-12 18:23:32 -0800

    implement type

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 #include <string>
+#include <unordered_set>
 
 void echo_command(std::string command) {
-    if (command.length() > 5) {
-      std::cout << command.substr(5) << '\n';
-    } else {
-      std::cout << '\n';
-    }
+  if (command.length() > 5) {
+    std::cout << command.substr(5) << '\n';
+  } else {
+    std::cout << '\n';
+  }
+}
+
+void type_command(
+    std::string command,
+    std::unordered_set<std::string> valid_commands
+) {
+  std::string command_query = command.substr(5);
+  if (valid_commands.find(command_query) != valid_commands.end()) {
+    std::cout << command_query << " is a shell builtin\n";
+  } else {
+    std::cout << command_query << ": not found\n";
+  }
 }
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
+  std::unordered_set<std::string> valid_commands;
+  valid_commands.insert("exit");
+  valid_commands.insert("echo");
+  valid_commands.insert("type");
 
   bool run = true;
   while (run) {
@@ -23,6 +41,8 @@ int main() {
       run = false;
     } else if (command.starts_with("echo")) {
       echo_command(command);
+    } else if (command.starts_with("type")) {
+      type_command(command, valid_commands);
     }
     else {
       std::cerr << command << ": command not found\n";
