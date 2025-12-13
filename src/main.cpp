bbe4bcf08ed28f648583d4bfe8d9a9a3f0581dd3
Joe Kern <josephdanielkern@gmail.com>
2025-12-12 18:13:57 -0800

    implement echo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <string>
 
+void echo_command(std::string command) {
+    if (command.length() > 5) {
+      std::cout << command.substr(5) << '\n';
+    } else {
+      std::cout << '\n';
+    }
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -10,11 +18,15 @@ int main() {
   while (run) {
     std::cout << "$ ";
     std::string command;
-    std::cin >> command;
+    std::getline(std::cin, command);
     if (command == "exit") {
       run = false;
-    } else {
+    } else if (command.starts_with("echo")) {
+      echo_command(command);
+    }
+    else {
       std::cerr << command << ": command not found\n";
     }
   }
 }
+
